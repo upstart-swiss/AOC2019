abc63981b44848b08c0e021c309d61c285d90a00
upstart-swiss <chris.schafer@upstart.com>
2019-12-09 11:40:09 -0800

    Day 9 and spelling Immediate correctly

--- day_9/day_9.py
+++ day_9/day_9.py
@@ -19,6 +19,9 @@ class OpCode(ABC):
         self.kwargs = kwargs
         self.output = None
         self.ran = False
+        self.relative_base = 0 
+        if 'relative_base' in kwargs:
+            self.relative_base = kwargs['relative_base']
     
     @property
     @lru_cache()
@@ -42,15 +45,37 @@ class OpCode(ABC):
         self.modes = opstring[-3::-1]
         # print(f"Modes: {self.modes}")
 
-    def fetch_value(self, position, mode="0"):
+    def fetch_value(self, position, write=False, mode="0"):
         # Modes are 0 - position 1 - immediate
+        # Now 2 - Relative
         # print(f"Fetching {position} - {self._opcodes[position]}")
+        data_at_loc = self._opcodes[position]
         if self.modes:
             mode = self.modes[position - self.base_position - 1]
+        if mode == "1" and write:
+            raise Exception("Cannot write in Immediate mode.")
         if mode == "0":
-            return self._opcodes[self._opcodes[position]]
+            position = data_at_loc
         elif mode == "1":
-            return self._opcodes[position]
+            position = position
+        elif mode == "2":
+            position = self.relative_base + data_at_loc
+        else:
+            raise Exception(f'Mode {mode} not known')
+        if position >= len(self._opcodes):
+            self._resize_to(position)
+        if write:
+            return position
+        return self._opcodes[position]
+    
+    def set_value(self, position, data):
+        if position >= len(self._opcodes):
+            self._resize_to(position)
+        self._opcodes[position] = data
+        
+    def _resize_to(self, size):
+        extra_data = size - len(self._opcodes) + 1
+        self._opcodes.extend([0 for i in range(extra_data)])
     
     @property
     def param_1(self):
@@ -65,8 +90,23 @@ class OpCode(ABC):
     @property
     def param_3(self):
         position = self.base_position + 3
-        return self._opcodes[position]
+        return self.fetch_value(position)
 
+    @property
+    def write_param_1(self):
+        position = self.base_position + 1
+        return self.fetch_value(position, write=True)
+    
+    @property
+    def write_param_2(self):
+        position = self.base_position + 2
+        return self.fetch_value(position, write=True)
+
+    @property
+    def write_param_3(self):
+        position = self.base_position + 3
+        return self.fetch_value(position, write=True)
+    
     @property
     @abstractmethod
     def my_opcode(self):
@@ -76,28 +116,6 @@ class OpCode(ABC):
     @abstractmethod
     def operation(self):
         pass
-
-class OpCode1(OpCode):
-    # Add Params
-    @property
-    def my_opcode(self):
-        return 1
-
-    def operation(self):
-        # print(f"Param1 {self.param_1}, Param2 {self.param_2}, param3 {self.param_3} ")
-        self._opcodes[self.param_3] = self.param_1 + self.param_2
-        self.position += 4
-
-class OpCode2(OpCode):
-    # Multiply Params
-    @property
-    def my_opcode(self):
-        return 2
-            
-    def operation(self):
-        # print(f"Param1 {self.param_1}, Param2 {self.param_2}, param3 {self.param_3} ")
-        self._opcodes[self.param_3] = self.param_1 * self.param_2
-        self.position += 4
     
 class OpCode99(OpCode):
     # Halt
@@ -108,19 +126,45 @@ class OpCode99(OpCode):
     def operation(self):
         self.should_continue = False
 
+
+class OpCode1(OpCode):
+    # Add Params
+    @property
+    def my_opcode(self):
+        return 1
+
+    def operation(self):
+        # print(f"Param1 {self.param_1}, Param2 {self.param_2}, param3 {self.param_3} ")
+        self.set_value(self.write_param_3, self.param_1 + self.param_2)
+        self.position += 4
+
+class OpCode2(OpCode):
+    # Multiply Params
+    @property
+    def my_opcode(self):
+        return 2
+            
+    def operation(self):
+        # print(f"Param1 {self.param_1}, Param2 {self.param_2}, param3 {self.param_3} ")
+        self.set_value(self.write_param_3, self.param_1 * self.param_2)
+        self.position += 4
+
 class OpCode3(OpCode):
     takes_input = True
     # Input
     @property
     def my_opcode(self):
         return 3
-    
+
     def operation(self):
         # Store a value provided to position
         this_input = int(self.kwargs["input"]) if "input" in self.kwargs else int(input("Program input: "))
-        # print(f"putting {this_input} in position {self.param_1}")
-        self.modes = "111"
-        self._opcodes[self.param_1] = this_input
+        # print(f"Modes: {self.modes}")
+        # print(f"putting {this_input} in position {self.write_param_1}")
+        # print(f"Relative Base: {self.relative_base}")
+        # print(f"{self._opcodes[self.position:self.position+2]}")
+        self.set_value(self.write_param_1, this_input)
+        # print(f"POsition {self.write_param_1}: {self._opcodes[self.write_param_1]}")
         self.position += 2
 
 class OpCode4(OpCode):
@@ -170,7 +214,7 @@ class OpCode7(OpCode):
             output = 1
         else:
             output = 0
-        self._opcodes[self.param_3] = output
+        self.set_value(self.write_param_3, output)
         self.position += 4
 
 class OpCode8(OpCode):
@@ -184,9 +228,19 @@ class OpCode8(OpCode):
             output = 1
         else:
             output = 0
-        self._opcodes[self.param_3] = output
+        self.set_value(self.write_param_3, output)
         self.position += 4
     
+class OpCode9(OpCode):
+    # Relative Base Modification
+    @property
+    def my_opcode(self):
+        return 9
+    
+    def operation(self):
+        self.relative_base += self.param_1
+        self.position += 2
+
 class OpCodeProcessor:
     opcode_runners = {
         1: OpCode1,
@@ -197,6 +251,7 @@ class OpCodeProcessor:
         6: OpCode6,
         7: OpCode7,
         8: OpCode8,
+        9: OpCode9,
         99: OpCode99
     }
     def __init__(self, opcodes, inputs=[], interactive=False, **kwargs):
@@ -207,6 +262,7 @@ class OpCodeProcessor:
         self.complete = False
         self.interactive = interactive
         self.outputs = []
+        self.relative_base = 0
     
     def run(self):
         while self.position < len(self.opcodes):
@@ -217,19 +273,20 @@ class OpCodeProcessor:
             runner = self.opcode_runners[code % 100]
             # print(f"223 is {self._opcodes[223]}")
             # print(f"224 is {self._opcodes[224]}")
-            # print(f"running pos {self.position} - code {code} - {self._opcodes[self.position:self.position + 4]}")
+            #print(f"running pos {self.position} - code {code} - {self.opcodes[self.position:self.position + 4]}")
             if runner.takes_input and self.inputs != []:
-                op = runner(self.opcodes, self.position, input=self.inputs.pop(0))
+                op = runner(self.opcodes, self.position, input=self.inputs.pop(0), relative_base=self.relative_base)
             elif runner.takes_input and self.interactive is False:
                 # Pause until re-activated
                 return
             else:
-                op = runner(self.opcodes, self.position)
+                op = runner(self.opcodes, self.position, relative_base=self.relative_base)
             op.run()
             self.opcodes = op.opcodes
             self.position = op.position
             if op.output is not None:
                 self.outputs.append(op.output)
+            self.relative_base = op.relative_base
             if op.should_continue is False:
                 self.complete = True
                 return
@@ -286,6 +343,24 @@ class AmplifierRunner:
             self.phase_ouputs.append(prev_output)
         self.continue_running()
 
+def get_highest_signal(opcodes, feedback=False):
+    highest = 0
+    highest_combo = None
+    combo_map = {}
+    items = [0,1,2,3,4] if feedback is False else [5,6,7,8,9]
+    for i in itertools.permutations(items):
+        
+        ar = AmplifierRunner(opcodes)
+        if feedback is False:
+            ar.run_sequence(i)
+        if feedback is True:
+            ar.run_feedback(i)
+        combo_map[ar.output] = i
+        if ar.output > highest:
+            highest_combo = i
+            highest = ar.output
+    # print(combo_map)
+    return highest, highest_combo
 
 
 def test_day2():
@@ -330,32 +405,25 @@ def test_day7_pt2():
     assert highest == 18216
     assert combo == (9,7,8,5,6)
 
-def get_highest_signal(opcodes, feedback=False):
-    highest = 0
-    highest_combo = None
-    combo_map = {}
-    items = [0,1,2,3,4] if feedback is False else [5,6,7,8,9]
-    for i in itertools.permutations(items):
-        
-        ar = AmplifierRunner(opcodes)
-        if feedback is False:
-            ar.run_sequence(i)
-        if feedback is True:
-            ar.run_feedback(i)
-        combo_map[ar.output] = i
-        if ar.output > highest:
-            highest_combo = i
-            highest = ar.output
-    # print(combo_map)
-    return highest, highest_combo
 
-day_7_codes = [3,8,1001,8,10,8,105,1,0,0,21,34,51,64,73,98,179,260,341,422,99999,3,9,102,4,9,9,1001,9,4,9,4,9,99,3,9,1001,9,4,9,1002,9,3,9,1001,9,5,9,4,9,99,3,9,101,5,9,9,102,5,9,9,4,9,99,3,9,101,5,9,9,4,9,99,3,9,1002,9,5,9,1001,9,3,9,102,2,9,9,101,5,9,9,1002,9,2,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,99,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,99]
+def test_day9_pt1():
+    input_1 = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
+    runner_1 = ImmediateProcessor(input_1)
+    assert runner_1.outputs == input_1
+    input_2 = [1102,34915192,34915192,7,4,7,99,]
+    assert len(str(ImmediateProcessor(input_2).outputs[0])) == 16
+    assert ImmediateProcessor([104,1125899906842624,99]).outputs[0] == 1125899906842624
+
+
+day_9_codes = [1102,34463338,34463338,63,1007,63,34463338,63,1005,63,53,1101,3,0,1000,109,988,209,12,9,1000,209,6,209,3,203,0,1008,1000,1,63,1005,63,65,1008,1000,2,63,1005,63,904,1008,1000,0,63,1005,63,58,4,25,104,0,99,4,0,104,0,99,4,17,104,0,99,0,0,1102,1,31,1008,1101,682,0,1027,1101,0,844,1029,1102,29,1,1001,1102,1,22,1014,1101,0,21,1011,1102,428,1,1025,1101,0,433,1024,1101,0,38,1019,1102,1,37,1016,1102,35,1,1017,1102,39,1,1018,1102,32,1,1000,1102,23,1,1012,1102,1,329,1022,1102,26,1,1006,1102,1,24,1003,1102,28,1,1005,1102,36,1,1010,1102,34,1,1004,1101,0,1,1021,1102,326,1,1023,1101,33,0,1015,1101,20,0,1002,1101,0,25,1007,1101,0,853,1028,1102,27,1,1009,1102,1,30,1013,1101,689,0,1026,1102,1,0,1020,109,12,2108,30,-3,63,1005,63,201,1001,64,1,64,1105,1,203,4,187,1002,64,2,64,109,-9,2101,0,6,63,1008,63,29,63,1005,63,227,1001,64,1,64,1106,0,229,4,209,1002,64,2,64,109,-6,1208,5,22,63,1005,63,249,1001,64,1,64,1106,0,251,4,235,1002,64,2,64,109,13,21107,40,41,8,1005,1018,273,4,257,1001,64,1,64,1105,1,273,1002,64,2,64,109,-11,2102,1,8,63,1008,63,25,63,1005,63,299,4,279,1001,64,1,64,1105,1,299,1002,64,2,64,109,15,1205,7,317,4,305,1001,64,1,64,1105,1,317,1002,64,2,64,109,10,2105,1,-1,1105,1,335,4,323,1001,64,1,64,1002,64,2,64,109,-22,1202,1,1,63,1008,63,24,63,1005,63,357,4,341,1106,0,361,1001,64,1,64,1002,64,2,64,109,13,1206,6,373,1106,0,379,4,367,1001,64,1,64,1002,64,2,64,109,11,1206,-6,393,4,385,1105,1,397,1001,64,1,64,1002,64,2,64,109,-32,1208,10,34,63,1005,63,419,4,403,1001,64,1,64,1105,1,419,1002,64,2,64,109,30,2105,1,0,4,425,1106,0,437,1001,64,1,64,1002,64,2,64,109,-28,1207,6,21,63,1005,63,455,4,443,1106,0,459,1001,64,1,64,1002,64,2,64,109,4,2101,0,8,63,1008,63,31,63,1005,63,485,4,465,1001,64,1,64,1105,1,485,1002,64,2,64,109,5,1207,-4,28,63,1005,63,505,1001,64,1,64,1106,0,507,4,491,1002,64,2,64,109,9,21102,41,1,2,1008,1016,39,63,1005,63,531,1001,64,1,64,1106,0,533,4,513,1002,64,2,64,109,-10,1201,4,0,63,1008,63,30,63,1005,63,553,1106,0,559,4,539,1001,64,1,64,1002,64,2,64,109,19,21108,42,41,-4,1005,1019,579,1001,64,1,64,1106,0,581,4,565,1002,64,2,64,109,-26,1201,3,0,63,1008,63,32,63,1005,63,607,4,587,1001,64,1,64,1106,0,607,1002,64,2,64,109,20,1205,3,623,1001,64,1,64,1105,1,625,4,613,1002,64,2,64,109,2,21107,43,42,-1,1005,1018,645,1001,64,1,64,1106,0,647,4,631,1002,64,2,64,109,-11,2102,1,1,63,1008,63,29,63,1005,63,667,1105,1,673,4,653,1001,64,1,64,1002,64,2,64,109,27,2106,0,-8,1001,64,1,64,1105,1,691,4,679,1002,64,2,64,109,-25,2107,25,-4,63,1005,63,713,4,697,1001,64,1,64,1105,1,713,1002,64,2,64,109,-2,21108,44,44,2,1005,1010,735,4,719,1001,64,1,64,1106,0,735,1002,64,2,64,109,11,21101,45,0,-3,1008,1016,45,63,1005,63,757,4,741,1106,0,761,1001,64,1,64,1002,64,2,64,109,-15,1202,3,1,63,1008,63,22,63,1005,63,781,1105,1,787,4,767,1001,64,1,64,1002,64,2,64,109,6,21101,46,0,0,1008,1010,49,63,1005,63,811,1001,64,1,64,1105,1,813,4,793,1002,64,2,64,109,-7,2108,34,1,63,1005,63,835,4,819,1001,64,1,64,1105,1,835,1002,64,2,64,109,15,2106,0,10,4,841,1001,64,1,64,1106,0,853,1002,64,2,64,109,-25,2107,33,7,63,1005,63,873,1001,64,1,64,1106,0,875,4,859,1002,64,2,64,109,7,21102,47,1,10,1008,1010,47,63,1005,63,897,4,881,1105,1,901,1001,64,1,64,4,64,99,21102,1,27,1,21102,915,1,0,1105,1,922,21201,1,12038,1,204,1,99,109,3,1207,-2,3,63,1005,63,964,21201,-2,-1,1,21102,942,1,0,1105,1,922,21202,1,1,-1,21201,-2,-3,1,21101,0,957,0,1106,0,922,22201,1,-1,-2,1106,0,968,22101,0,-2,-2,109,-3,2105,1,0]
 if __name__ == "__main__":
-    # op = OpCodeProcessor(day_5_input)
-    # op.run()
-    highest, combo = get_highest_signal(day_7_codes)
-    print(f"HIghest combo is {highest} from {combo}")
-    highest, combo = get_highest_signal(day_7_codes, True)
-    print(f"HIghest feedback combo is {highest} from {combo}")
-
-
+    runner_1 = OpCodeProcessor(day_9_codes, inputs=["1"])
+    runner_1.run()
+    pt1_answer = runner_1.outputs[0]
+    if pt1_answer < 99999:
+        print(f"Failed pt 1 - {pt1_answer}")
+    else:
+        print(f"Pt1: {pt1_answer}")
+    runner_2 = OpCodeProcessor(day_9_codes, inputs=["2"])
+    runner_2.run()
+    print(f"Pt2 Answer: {runner_2.outputs[0]}")
